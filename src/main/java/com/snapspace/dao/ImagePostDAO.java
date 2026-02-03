7c578b2ec8af497ed449af338202a0cc0dbf1c2a
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-02-03 08:21:40 +0200

    User and Image doa's

--- src/main/java/com/snapspace/dao/ImagePostDAO.java
+++ src/main/java/com/snapspace/dao/ImagePostDAO.java
@@ -1,4 +1,28 @@
 package com.snapspace.dao;
 
+import com.snapspace.model.ImagePost;
+import com.snapspace.config.HibernateUtil;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
 public class ImagePostDAO {
+
+    private final SessionFactory sf = HibernateUtil.getSessionFactory();
+
+    public void save(ImagePost post) {
+        try (Session s = sf.openSession()) {
+            s.beginTransaction();
+            s.persist(post);
+            s.getTransaction().commit();
+        }
+    }
+
+    public List<ImagePost> findAll() {
+        try (Session s = sf.openSession()) {
+            return s.createQuery("from ImagePost", ImagePost.class).list();
+        }
+    }
 }
--- src/main/java/com/snapspace/dao/UserDAO.java
+++ src/main/java/com/snapspace/dao/UserDAO.java
@@ -1,4 +1,31 @@
 package com.snapspace.dao;
 
+import com.snapspace.model.User;
+import com.snapspace.config.HibernateUtil;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 public class UserDAO {
+
+    private final SessionFactory sf = HibernateUtil.getSessionFactory();
+
+    public void save(User user) {
+        try (Session session = sf.openSession()) {
+            session.beginTransaction();
+            session.persist(user);
+            session.getTransaction().commit();
+        }
+    }
+
+    public User findByEmail(String email) {
+        try (Session session = sf.openSession()) {
+            return session.createQuery(
+                            "from User where email = :email",
+                            User.class
+                    )
+                    .setParameter("email", email)
+                    .uniqueResult();
+        }
+    }
 }
