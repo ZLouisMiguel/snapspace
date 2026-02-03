6b1e0e39cc03ae18d5a3b0aabf8307f326125ff8
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-02-03 07:54:23 +0200

    Auth and image services

--- src/main/java/com/snapspace/service/AuthService.java
+++ src/main/java/com/snapspace/service/AuthService.java
@@ -1,4 +1,22 @@
 package com.snapspace.service;
 
+import com.snapspace.model.User;
+import com.snapspace.dao.UserDAO;
+import com.snapspace.util.PasswordUtil;
+
 public class AuthService {
+
+    private final UserDAO userDAO = new UserDAO();
+
+    public User login(String email, String password) {
+        User u = userDAO.findByEmail(email);
+        if (u != null && PasswordUtil.check(password, u.getPasswordHash())) {
+            return u;
+        }
+        return null;
+    }
+
+    public void register(User user) {
+        userDAO.save(user);
+    }
 }
--- src/main/java/com/snapspace/service/ImageService.java
+++ src/main/java/com/snapspace/service/ImageService.java
@@ -1,4 +1,19 @@
 package com.snapspace.service;
 
+import com.snapspace.model.ImagePost;
+import com.snapspace.dao.ImagePostDAO;
+
+import java.util.List;
+
 public class ImageService {
+
+    private final ImagePostDAO dao = new ImagePostDAO();
+
+    public void upload(ImagePost post) {
+        dao.save(post);
+    }
+
+    public List<ImagePost> feed() {
+        return dao.findAll();
+    }
 }
