0cb1ecdd5879f41f0c7c1fa890277e9e5a8d0ad6
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-02-05 09:14:29 +0200

    Added simple javadoc for the User class

--- src/main/java/com/snapspace/model/User.java
+++ src/main/java/com/snapspace/model/User.java
@@ -5,55 +5,109 @@ import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.time.Period;
 
+/**
+ * This is the class that represents the User entity in the SnapSpace application
+ *
+ * @author Louis Miguel , GitHub -username: ZLouisMiguel
+ *
+ */
+
+
 @Entity
 @Table(name = "users")
 public class User {
-
+    /**
+     * Unique Identifier for each user
+     */
     @Id
     @GeneratedValue
     private Long id;
 
+    /**
+     * Unique email address for each user
+     */
     @Column(unique = true, nullable = false)
     private String email;
 
+    /**
+     * Username chosen by the user for public display in the system.
+     */
     private String username;
 
+    /**
+     * Hashed password for the user saved in the db
+     */
     private String passwordHash;
 
-
+    /**
+     * Default constructor for the class
+     */
     public User() {
     }
 
-    // ----- Getters -----
+
+    /**
+     * getId method public getter for user id
+     *
+     * @return user's Id
+     */
     public Long getId() {
         return id;
     }
+
+    /**
+     * getEmail method public getter for user email
+     *
+     * @return user_email
+     */
 
     public String getEmail() {
         return email;
     }
 
+    /**
+     * getUsername method public getter for user's chosen username
+     *
+     * @return username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * getPasswordHash method public getter for the stored user's password hash
+     *
+     * @return passwordHash
+     */
     public String getPasswordHash() {
         return passwordHash;
     }
 
-    // ----- Setters -----
-    public void setId(Long id) {
-        this.id = id;
-    }
-
+    /**
+     * Sets the email address of the user.
+     *
+     * @param email the user's email address
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+
+    /**
+     * Sets the username of the user.
+     *
+     * @param username the user's username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+
+    /**
+     * Sets the hashed password of the user.
+     *
+     * @param passwordHash the hashed password
+     */
     public void setPasswordHash(String passwordHash) {
         this.passwordHash = passwordHash;
     }
