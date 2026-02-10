b2602418322b8ba5ae353964df4d69f80a87170f
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-02-10 07:34:26 +0200

    Tried documenting the code for imagepost class using javadoc

--- src/main/java/com/snapspace/model/ImagePost.java
+++ src/main/java/com/snapspace/model/ImagePost.java
@@ -2,40 +2,83 @@ package com.snapspace.model;
 
 import jakarta.persistence.*;
 
+/**
+ * This is the class that represents the ImagePost entity in the SnapSpace application
+ *
+ * @author Louis Miguel , @see <a href="https://github.com/ZLouisMiguel #value">label</a>
+ *
+ */
+
 @Entity
 @Table(name = "image_posts")
 public class ImagePost {
-
+    /**
+     * Unique identifier for each image post uploaded
+     */
     @Id
     @GeneratedValue
     private Long id;
 
+    /**
+     * Image Post title
+     */
     private String title;
+
+    /**
+     * Path to find the image_post
+     */
     private String imagePath;
 
+
+    /**
+     * Image Post owner
+     * {@link com.snapspace.model.User} User entity
+     */
     @ManyToOne
     private User owner;
 
+
+    /**
+     * getId method public getter for the imagePost id
+     * @return id;
+     */
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
+    /**
+     * getTitle method returns the image post's title
+     * @return title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * setTitle method public setter for the image post's title
+     * @param title image post's title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+
+    /**
+     * getImagePath public getter for the image post's saved path
+     * @return imagePath
+     */
+
     public String getImagePath() {
         return imagePath;
     }
+
+
+    /**
+     * getImagePath public getter for the image post's saved path
+     * @param  imagePath path to image post;
+     */
 
     public void setImagePath(String imagePath) {
         this.imagePath = imagePath;
